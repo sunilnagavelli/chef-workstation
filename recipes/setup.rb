97acfc26e318f69ade8e29ab812a220932ea7e98
vagrant <vagrant@vagrant.vm>
2018-08-15 11:21:51 +0000

    Initial commit

---
+++ recipes/setup.rb
@@ -0,0 +1,10 @@
+#!/bin/bash ruby
+# Cookbook 'chef-workstation'
+# Recipie:: setup
+#
+# Copyright:: 2018, Sunil, All rights reserved.
+
+package 'tree'
+package 'git'
+package 'net-tools'
+package 'iputils-ping'
